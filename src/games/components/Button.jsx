0aebb077fa94b734a2b9bfc7e0f5dc1ee72b6d0d
Remi Tondeux <remi.tondeux@gmail.com>
2025-05-26 11:39:06 +0200

    project react9tq 100%

--- src/games/components/Button.jsx
+++ src/games/components/Button.jsx
@@ -1,5 +1,7 @@
 export function Button({onClick, isGameStarted}) {
   if (!isGameStarted) {
     return <button className="btn-dark" onClick={onClick}>Jouer</button>
+  } else {
+    return <div></div>
   }
 }
